be40577639db3ab596ff6f03c0909478a179c9a7
felipepolachini <fpiandoli@gmail.com>
2020-03-11 09:47:38 -0300

    Update search_page.rb

--- qa-junior-pleno/automation/features/pages/search_page.rb
+++ qa-junior-pleno/automation/features/pages/search_page.rb
@@ -4,17 +4,17 @@ class SearchPage < SitePrism::Page
      set_url "/?s=#{$articleTitle}"
 
      def self.checkUrl
-        expect(page).to have_current_path('/?s=#{$articleTitle}', url: true)
+        return expect(page).to have_current_path('/?s=#{$articleTitle}', url: true)
       end
 
      def self.checkTitleBar
         element :titleBar,:xpath, '//h1[text()= "#{$articleTitle}"]'
-        has_titleBar
+        return has_titleBar
      end
 
      def self.checkArticleTitle
        element :articleTitle,:xpath, '//a[text()= "#{$articleTitle}"]'
-       has_articleTitle
+       return has_articleTitle
      end    
     
 end
